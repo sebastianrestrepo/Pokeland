40e771615f387aab5eb588f7c3b3bbabe866ec7c
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2018-10-17 23:34:54 -0500

    Hello :octocat:
    Init

---
+++ src/Mundo.java
@@ -0,0 +1,11 @@
+import processing.core.PApplet;
+
+public class Mundo {
+
+	public Mundo(PApplet app) {
+		
+	}
+	
+	
+	
+}
---
+++ src/PokelandMain.java
@@ -0,0 +1,34 @@
+import processing.core.PApplet;
+
+public class PokelandMain extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("PokelandMain");
+	}
+
+	@Override
+	public void settings() {
+		size(1200, 700);
+	}
+
+	@Override
+	public void setup() {
+
+	}
+
+	@Override
+	public void draw() {
+
+	}
+
+	@Override
+	public void mouseClicked() {
+
+	}
+
+	@Override
+	public void mouseDragged() {
+
+	}
+
+}
